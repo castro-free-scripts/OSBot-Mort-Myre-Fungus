78dd05e562fb92f81a6a600c2b0a67d1ff7c8510
castro-free-scripts <68300551+castro-free-scripts@users.noreply.github.com>
2020-07-16 13:05:40 +0100

    v0.14
    Updated to support new Clan wars area
    Added banking task back

--- com/castro/mmf/main/Main.java
+++ com/castro/mmf/main/Main.java
@@ -4,6 +4,7 @@ import com.castro.mmf.data.Location;
 import com.castro.mmf.data.Painting;
 import com.castro.mmf.data.Setting;
 import com.castro.mmf.main.framework.Task;
+import com.castro.mmf.tasks.Banking;
 import com.castro.mmf.tasks.Pick;
 import org.osbot.rs07.api.ui.Message;
 import org.osbot.rs07.canvas.paint.Painter;
@@ -28,7 +29,7 @@ public class Main extends Script implements MessageListener, Painter {
             Setting.teleport = new String[]{"Law rune", "Soul rune"};
             Setting.usingRunes = true;
         }
-        //  tasks.add(new Banking(this));
+        tasks.add(new Banking(this));
         tasks.add(new Pick(this));
     }
 
